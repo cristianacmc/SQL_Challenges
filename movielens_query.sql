3d83fdc1aeb826347f3271b6ffe674b109e9c786
cristianacmc <cmdasilvacosta@gmail.com>
2022-10-26 15:44:37 +0100

    corrected third question

--- movielens_query.sql
+++ movielens_query.sql
@@ -17,12 +17,21 @@ ORDER BY r.rating
 -- 3 - List the unique records for Sci-Fi movies where male 40-year-old students have given 5-star ratings.
 SELECT DISTINCT *
 from movies m
-NATURAL JOIN genres_movies gm
-NATURAL JOIN genres g
-NATURAL JOIN ratings r
-NATURAL JOIN users u
+INNER JOIN genres_movies gm
+	ON m.id = gm.movie_id
+INNER JOIN genres g
+	ON gm.genre_id = g.id
+INNER JOIN ratings r
+	ON m.id = r.movie_id
+INNER JOIN users u
+	ON r.user_id = u.id
 WHERE g.name = "Sci-Fi"
 AND r.rating = 5
 AND u.gender = "M"
 AND u.age = 40;
- 
+
+-- 4 - List the unique titles of each of the movies released on the most popular release day.
+/*SELECT DISTINCT title 
+FROM movies 
+WHERE 
+ */
