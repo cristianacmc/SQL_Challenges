874358b3399bd889eeef6c0f993ebe5548593325
cristianacmc <cmdasilvacosta@gmail.com>
2022-10-26 15:14:50 +0100

    question2

--- movielens_query.sql
+++ movielens_query.sql
@@ -1,6 +1,14 @@
 USE movielens;
-
+-- List the titles and release dates of movies released between 1983-1993 in reverse chronological order.
+/*
 SELECT title, release_date
 FROM movies 
-WHERE release_date BETWEEN "1983" AND "1993"
+WHERE release_date BETWEEN "1983-01-01" AND "1993-12-31"
 ORDER BY release_date;
+*/
+
+-- Without using LIMIT, list the titles of the movies with the lowest average rating.
+SELECT m.title, r.rating 
+FROM movies m
+NATURAL JOIN ratings r
+ORDER BY r.rating 
